a334dfeff0255412a382d0e677591545b73f86fd
Kianoosh Afshari <112182609+llkianooshll@users.noreply.github.com>
2024-07-15 03:42:11 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-let toggle = false;
-        document.querySelector(".slider").addEventListener("click",()=>{
-            if(toggle == false){
-                document.querySelector("body").style.backgroundColor = "black";
-                document.querySelectorAll(".changer").forEach((item)=>{item.classList.replace("changer","switch_mode")})
-                toggle = true;
-            }else{
-                document.querySelector("body").style.backgroundColor = "white";
-                document.querySelectorAll(".switch_mode").forEach((item)=>{item.classList.replace("switch_mode","changer")})
-                toggle = false
-            }
-        })
+
